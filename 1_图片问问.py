2610bea31e0aeb45e9da4b3a69955f16c21265e1
xiaogenliu <xiaogenliu@kugou.net>
2023-12-18 14:49:00 +0800

    update

--- 1_图片问问.py
+++ 1_图片问问.py
@@ -19,7 +19,6 @@ model = genai.GenerativeModel('gemini-pro-vision')
 
 st.title('上传图片问问')
 
-
 def show_message(prompt, image, loading_str):
     with st.chat_message("assistant"):
         message_placeholder = st.empty()
@@ -53,28 +52,22 @@ def clear_state():
     st.session_state.history_pic = []
 
 image = None
-uploaded_file = st.file_uploader("选择一张图片...", type=["jpg", "png", "jpeg", "gif", "bmp"], label_visibility='collapsed', on_change = clear_state)
+uploaded_file = st.file_uploader("选择一张图片...", type=["jpg", "png", "jpeg", "gif"], label_visibility='collapsed', on_change = clear_state)
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
     st.image(image)    
 
 if len(st.session_state.history_pic) == 0 and image is not None:
     prompt = """##### 角色
-你是一位出色的影像解读者，擅长从图片中解读细节并能为其创作详尽的描述。你也会提供三个引导问题，引导问题可以帮助用户以不同的方式利用你的图片描述 —— 像是创作微信朋友圈文案、微信公众号文章，小红书推文或商品详细页面的说明。
+你是一位出色的影像解读者，擅长从图片中解读细节并能为其创作详尽的描述。
 
 ##### 技能
-###### 技能1: 图片解读和描述
 - 分析图片，挖掘图片背后的故事以及图片展现出来的氛围和意境。
 - 基于图片内容，创作出详尽、引人入胜的文字描述。
 
-###### 技能2: 创建引导问题
-- 提供三个引导问题，题型风格包括但不限于生成微信朋友圈文案、微信公众号文章，小红书推文或商品详细页面。
-- 通过引导问题，助用户更好的将图片描述应用于不同场合。
-
 ##### 限制条件
 - 描述与图片应紧密相连，不偏离图片本身的内容。
-- 描述应尽可能详实，使读者能通过文字理解图片的魅力。
-- 描述应适合作为微信朋友圈文案、微信公众号文章，小红书推文、商品详细页说明的创作素材。"""
+- 描述应尽可能详实，使读者能通过文字理解图片的魅力。"""
     show_message(prompt, image, "正在解读图片内容...")
     
 else:
